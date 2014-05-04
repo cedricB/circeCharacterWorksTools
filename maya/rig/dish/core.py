c13fdd289c1339532ab44d95e4db03e6d7186f7f
cedric bazillou <cedricbazillou@gmail.com>
2014-05-04 15:47:31 -0500

    clean up some dish code

--- maya/rig/dish/core.py
+++ maya/rig/dish/core.py
@@ -9,26 +9,82 @@ version = 0.02
 
 class UI:
     def __init__(self):
-        self.windowRef ='CCW_templateUI'
+        self.windowRef ='CCW_bentoUI'
         self.tmpleLib =  IO()
         self.canvasSize =[430,400]
         self.UI_TAB = ''
         self.ToDelete = []
-    def CompileTab(self):
-        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.UI_TAB)
+        self.toresizeCtrl = ''
+        self.toresizeCtrlB = ''
+    def resizeTab_In_Compile(self):
+        tabIndex = mc.tabLayout(self.toresizeCtrl ,q=True,sti=True )
+        if tabIndex == 1 :
+             mc.tabLayout(self.toresizeCtrl,e=True,  h=592 )
+        if tabIndex == 2 :
+            mc.tabLayout(self.toresizeCtrl,e=True,  h=80 )
+    def injectElement_Tab(self ):
+        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.toresizeCtrlB)
+        mc.button(l='Scan current scene content')
+        mc.textScrollList( 'CCW_TOOLS_bentoElem',numberOfRows=10, allowMultiSelection=True)
+        mc.button(l='Remove selected',h=36)
+        
+    def defineDriver_Tab(self, ):
+        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.toresizeCtrlB)
+        mc.separator()
+    def flagRoot_tab(self):
+        mc.text(l='FoodType')
+        mc.textField( 'CCW_TOOLS_FoodType_txFld', )
+        mc.separator()
+        mc.text(l='Description')
+        mc.scrollField( editable=True, wordWrap=True, text='#Any relevant informations can be placed here' )
+        mc.button(l='Flag bento root')
+        mc.separator()
+    def bundleTab(self):
+        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.toresizeCtrl)
         mc.button(l='Compile bundle',c=self.tmpleLib.compile_bundle)
         mc.progressBar('CCW_TOOLS_bundle_prgrss')
-    def leftPanel_in_templateListing(self,anchor):
+        
+        
+        mc.tabLayout(self.toresizeCtrl ,e=True,tabLabelIndex=[1,'Cook'])
+        mc.tabLayout(self.toresizeCtrl ,e=True,tabLabelIndex=[2,'Deliver'])
+        mc.tabLayout(self.toresizeCtrl ,e=True,cc=self.resizeTab_In_Compile )
+    def CompileTab(self):
+        mc.text(l=' Current Path :')
+        pathTxFld = mc.textField( 'data_dir_txFld',ed=False  )
+        self.toresizeCtrl = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5,p=self.UI_TAB  )
+        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.toresizeCtrl)
+        anchorA = mc.frameLayout( collapsable=False,labelVisible=False, mw=5,mh=5,borderStyle='out' )
+        
+        self.flagRoot_tab()        
+        self.toresizeCtrlB = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5 )        
+        self.injectElement_Tab() 
+        self.defineDriver_Tab()
+
+        
+        mc.setParent(anchorA)
+        mc.separator()
+        mc.button(l='Release bento',h=45)
+        
+        #---------------------------------------------------------------------------------
+        self.bundleTab()
+        
+
+        modulePath = ''.join(os.path.dirname(inspect.getfile(self.bentoListing))+'\src')
+        mc.textField( pathTxFld,e=True,tx=modulePath)
+        
+        mc.tabLayout(self.toresizeCtrlB ,e=True,tabLabelIndex=[1,'Flag Elements'])
+        mc.tabLayout(self.toresizeCtrlB ,e=True,tabLabelIndex=[2,'Expose Drivers'])
+    def leftPanel_in_bentoListing(self,anchor):
         leftPanel = mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5,p=anchor )
         mc.columnLayout( adjustableColumn=True, rs=5 )
-        mc.text(l='Available Template')
+        mc.text(l='Available Bentos')
         mc.scrollLayout(	horizontalScrollBarThickness=0,verticalScrollBarThickness=8)
         scrollWdth = mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0]/5*2-27,h=self.canvasSize[1]-20)
         bndList = self.tmpleLib.exposeZipTemplate()
         for n in range(len(bndList) ):
             templateCls = os.path.basename(bndList[n]).split('.zip')[0]
             mc.button(l=templateCls,h=28,ann='test')
-    def rigthPanel_in_templateListing(self,anchor):
+    def rigthPanel_in_bentoListing(self,anchor):
         rgtTab = mc.tabLayout( p=anchor,innerMarginWidth=5, innerMarginHeight=5,h=self.canvasSize[1]-52)
         mc.frameLayout(   mw=5,labelVisible=False,mh=5,p=rgtTab )
         mc.text(l='')
@@ -43,9 +99,9 @@ class UI:
         
         #######################################################################
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[1,'Infos'])
-        mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[2,'Member list'])
-        mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[3,'Tools'])
-    def templateListing(self):
+        mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[2,'Tools'])
+        mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[3,'Member list'])
+    def bentoListing(self):
         mc.columnLayout( adjustableColumn=True, rs=5 ,p=self.UI_TAB)
         mc.rowLayout( numberOfColumns=3, columnWidth2=(80,(self.canvasSize[0]-80-20)), adjustableColumn=2,cl2=('left','both' )  )
         mc.text(l=' Current Path :')
@@ -54,23 +110,25 @@ class UI:
         mc.separator()
         listDock = mc.rowLayout( numberOfColumns=2, columnWidth2=(self.canvasSize[0]/5*2+12, 75 ), adjustableColumn=2, columnAlign=(1, 'right'), columnAttach=[(1, 'both', 0), (2, 'both', 0)] ,w=self.canvasSize[0])
 
-        self.leftPanel_in_templateListing(listDock)
+        self.leftPanel_in_bentoListing(listDock)
         #------------------------------------------------------------------------------------------------------------
-        self.rigthPanel_in_templateListing(listDock)
+        self.rigthPanel_in_bentoListing(listDock)
         modulePath = ''.join(os.path.dirname(inspect.getfile(self.CompileTab)))
         mc.textField( pathTxFld,e=True,tx=modulePath)
     def show(self):
         if mc.window(self.windowRef,q=True,ex=True) == True:
             mc.deleteUI(self.windowRef)
-        mc.window(self.windowRef , title="CCW_Template Manager  %s"%version,  widthHeight=(self.canvasSize[0],self.canvasSize[1]),rtf=True,s=False )
+        mc.window(self.windowRef , title="CCW_Bento Manager  %s"%version,  widthHeight=(self.canvasSize[0],self.canvasSize[1]),rtf=True,s=False )
  
         self.UI_TAB = mc.frameLayout(cll=False,lv=False,mw=5,w=self.canvasSize[0])
         bndList = self.tmpleLib.exposeZipTemplate()
         if len(bndList) == 0:
             self.CompileTab()
         else:
-            self.templateListing()
+            self.bentoListing()
 
+        mc.textField( 'copyrigth_txFld',ed=False ,tx=' AUTHOR : cedric BAZILLOU 2014      --       http://circecharacterworks.wordpress.com/ ',p=self.UI_TAB  )
+        
         mc.showWindow( self.windowRef)
         mc.window( self.windowRef,e=True, widthHeight=(self.canvasSize[0],self.canvasSize[1]) )
 class IO:
