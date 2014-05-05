2567a13b023fad2828816cbe9d21f941918b38ea
cedric bazillou <cedricbazillou@gmail.com>
2014-05-05 03:13:26 -0500

    add zip support to read module date from

--- maya/rig/dish/core.py
+++ maya/rig/dish/core.py
@@ -4,12 +4,33 @@ import maya.OpenMaya as OpenMaya
 import  inspect
 import zipfile
 from functools import partial
+import json
 
+'''
+* ..change is common
+* ..space is cheap
+* ..control outweighs performance
+
+
+|Feature                          |Description  |
+|:--------------------------------|:------------|
+|Non-destructive                  | Every change you make is maintained in history; facilitating persistent undo/redo, traceable history of who did what and when, including incremental versioning with arbitrary metadata, like a changelog or description. All retrievable at any point in time. |
+|Full disclosure                  | You may at any point in time browse to data at any hierarchical level using your native file-browser; read, write, modify, delete or debug malicious data with tools you know.    |
+|Partial I/O                      | As a side-effect to its inherently simplistic design, reading and writing within large sets of data doesn't require reading everything into memory nor does it affect surrounding data; facilitating distributed collaborative editing. See [RFC13][] for more information.
+|No limits on size nor complexity | Store millions of strings, booleans, matrices.. groups of matrices.. matrices of groups, strings, booleans and vectors. On consumer hardware, in a matter of megabytes, without compression. Then go ahead and store billions.
+|Open specification, open source  | There are no mysteries about the inner-workings of the data that you write; you may write personal tools for debugging, graphical interfaces or extensions to the standard. The specifications are all laid out below and collaboration is welcome. (Want Open Metadata in Lua, Java, PHP or C++?)
+
+### 
+
+'''
 author = 'cedric bazillou 2013'
-version = 0.03
+version = 0.04
 
 class dishBuilder:
     def __init__(self):
+        self.scrollInfo  = ''
+        self.assetListing  = ''
+        self.bentoRoot = ''
         self.tabCtrl_A = ''
         self.tabCtrl_B = ''
         self.canvasSize =[430,400]
@@ -17,6 +38,7 @@ class dishBuilder:
         self.bentoElements = 'CCW_TOOLS_bentoElem'
         self.foodField = ''
         self.IO =  IO()
+        self.factory = factory()
         self.defaultNodes = [u'time1', u'sequenceManager1', u'renderPartition',
         u'renderGlobalsList1', u'defaultLightList1', u'defaultShaderList1',
         u'postProcessList1', u'defaultRenderUtilityList1',
@@ -36,12 +58,11 @@ class dishBuilder:
         u'objectTypeFilter78', u'objectScriptFilter10', u'renderingSetsFilter',
         u'objectTypeFilter79', u'relationshipPanel1LeftAttrFilter', u'relationshipPanel1RightAttrFilter',
         u'layerManager', u'defaultLayer', u'renderLayerManager', u'defaultRenderLayer'] # 
-
     #---------------------------------------------------------------------------------------- widget Helpers 
     def resizeBuildTabs(self):
         tabIndex = mc.tabLayout(self.tabCtrl_A ,q=True,sti=True )
         if tabIndex == 1 :
-             mc.tabLayout(self.tabCtrl_A,e=True,  h=618 )
+             mc.tabLayout(self.tabCtrl_A,e=True,  h=620 )
         if tabIndex == 2 :
             mc.tabLayout(self.tabCtrl_A,e=True,  h=80 )
     #---------------------------------------------------------------------------------------- MAIN UI 
@@ -63,41 +84,105 @@ class dishBuilder:
     #---------------------------------------------------------------------------------------- cookTab UI sub Elements
     def flagRoot_UI(self):
         mc.rowLayout( numberOfColumns=3, columnWidth3=(80,(self.canvasSize[0]-80-20),20 ), adjustableColumn=2,cl3=('left','both','right'))
-        mc.text(l=' Bento root :')
-        txFld = mc.textField( 'CCW_TOOLS_ROOT_data_dir_txFld' )
-        psdFileRootBtn = mc.button(l='<' ,c=partial(self.collect_root, txFld ) )
+        mc.text(l='1. Bento root :')
+        self.bentoRoot = mc.textField( 'CCW_TOOLS_ROOT_data_dir_txFld' )
+        psdFileRootBtn = mc.button(l='<' ,c=partial(self.collect_root,  self.bentoRoot ) )
         mc.setParent('..')
 
-        mc.text(l='FoodType')
+        mc.text(l='2. FoodType')
         self.foodField = mc.textField( 'CCW_TOOLS_FoodType_txFld',alwaysInvokeEnterCommandOnReturn=True )
         mc.textField( self.foodField,e=True, enterCommand=partial(self.validateModule, self.foodField  )  )
         mc.separator()
-        mc.text(l='Description')
-        mc.scrollField( editable=True, wordWrap=True, text='#Any relevant informations can be placed here' )
-        mc.button(l='Flag bento root')
+        mc.text(l='3. Description')
+        self.scrollInfo = mc.scrollField( editable=True, wordWrap=True, text='#Any relevant informations can be placed here' )
+        mc.button(l='Flag bento root',c=self.validateRootData)
         mc.separator()
     def injectElement_UI(self ):
-        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.tabCtrl_B)
-        mc.button(l='Scan current scene content',c=self.scanSceneGraph)
-        mc.textScrollList( 'CCW_TOOLS_bentoElem',numberOfRows=10, allowMultiSelection=True)
-        mc.button(l='Remove selected',h=36,c=self.removeNodeFromElementList)
+        slot = mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.tabCtrl_B)
+        anchorDock = mc.rowLayout(    numberOfColumns=2,
+                            columnWidth2=(self.canvasSize[0]/5*2-30, self.canvasSize[0]/5*2-20 ),
+                            adjustableColumn=1,
+                            columnAlign=(1, 'right'),
+                            columnAttach=[(1,'both',0), (2,'both',0)] ,w=self.canvasSize[0]-40)
+        mc.columnLayout( adjustableColumn=True  )
+        mc.button(l='4-a. Scan current scene content',c=self.scanSceneGraph)
+
+        mc.columnLayout( adjustableColumn=True,p=anchorDock    )
+        mc.button(l='4-b. Remove selected' ,c=self.removeNodeFromElementList)
+        
+        mc.setParent(slot)
+        self.assetListing  = mc.textScrollList( 'CCW_TOOLS_bentoElem',numberOfRows=10, allowMultiSelection=True )
+        mc.button(l='4-c. Inject Elements',h=36 ,c=self.validateDishData)
     def defineDriver_UI(self ):
         mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.tabCtrl_B)
-        mc.separator(style='none')
+        
+        anchorDock = mc.rowLayout(    numberOfColumns=2,
+                            columnWidth2=(self.canvasSize[0]/5*2, 95 ),
+                            adjustableColumn=2,
+                            columnAlign=(1, 'right'),
+                            columnAttach=[(1,'both',0), (2,'both',0)] ,w=self.canvasSize[0])
+        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedOut',mw=5,mh=5  )
+        mc.button(l='Scan Elements' )
+        mc.textScrollList( 'CCW_TOOLS_bentoDriver',numberOfRows=10, allowMultiSelection=True)
+        mc.button(l='Remove Elements' ,w=40)
+        
+        mc.text(l='driver element in this list',p=anchorDock)
     #---------------------------------------------------------------------------------------- cookTab UI Helpers
+    def validateDishBeforePublish(self,*args):
+        root = mc.textField( self.bentoRoot,q=True,tx=True)
+        erroChk = 0
+        if len(root)== 0:
+            print 'Please pick a root object'
+        else:
+            attributeList = ['foodType','moduleInfos','element']
+            feedBackList = ['found no foodType data in this dish','found no moduleInfos data in this dish','found no element data in this dish']
+            for index, checkObj in enumerate(attributeList): 
+                if mc.attributeQuery(checkObj,node=root,ex=True) == False:
+                    erroChk += 1
+                    mc.warning(feedBackList[index])
+            if erroChk == 0:
+                self.factory.publish_driver(root)
+                print 'publis succes'
+    def validateRootData(self,*args):
+        root = mc.textField( self.bentoRoot,q=True,tx=True)
+        foodType = mc.textField( self.foodField,q=True,tx=True)
+        erroChk = 0
+        chkList = [root,foodType]
+        feedBackList = ['Please pick a root object','Please define a unique foodType']
+        for index, checkObj in enumerate(chkList): 
+            if checkObj is None or len(checkObj)== 0:
+                erroChk += 1
+                mc.warning(feedBackList[index])
+        if erroChk == 0:
+            infoData = mc.scrollField( self.scrollInfo ,q=True,tx=True)
+            self.factory.process_root(  root,
+                                        foodType,
+                                        infoData )
+    def validateDishData(self,*args):
+        root = mc.textField( self.bentoRoot,q=True,tx=True)
+        assetListing = mc.textScrollList( self.assetListing  ,q=True,ai=True)
+        erroChk = 0
+        chkList = [root,assetListing]
+        feedBackList = ['Please pick a root object','Please Fill the Element List']
+        for index, checkObj in enumerate(chkList): 
+            if checkObj is None or len(checkObj)== 0:
+                erroChk += 1
+                mc.warning(feedBackList[index])
+        if erroChk == 0:
+            self.factory.expose_members(root,assetListing)
     def validateModule(self ,fieldRef,*args):
         foodTx = mc.textField( self.foodField,q=True,tx=True)
         if foodTx is not None:
             modulePath = ''.join(os.path.dirname(inspect.getfile(self.resizeBuildTabs))+'\src')
             filelist= os.listdir(modulePath)
             if foodTx in  filelist:
-            
                 prt = mc.promptDialog(    title='foodType Alert', 
                                     message='Please choose another foodType name:',
                                     button=['OK'],
                                     defaultButton='OK')
                 mc.textField( self.foodField,e=True,tx='')
-            
+            else:
+                OpenMaya.MGlobal.displayInfo( '** %s is valid foodType, Please fill the other UI part to proceed'% foodTx )
     def collect_root(self,fieldRef,*args):
         sel = mc.ls(sl=True,fl=True)
         mc.textField(fieldRef,e=True,tx='')
@@ -132,13 +217,13 @@ class dishBuilder:
         # publish Elements
         mc.setParent(buildDish_UI_Anchor)
         mc.separator()
-        mc.button(l='Release bento',h=45)
+        mc.button(l='Release bento',h=45,c=self.validateDishBeforePublish)
         
-        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[1,'Flag Elements'])
-        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[2,'Expose Drivers'])
+        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[1,'4. Flag Elements'])
+        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[2,'5. Expose Drivers'])
     #---------------------------------------------------------------------------------------- deliverTab UI Helpers
     
-    #---------------------------------------------------------------------------------------- cook TAB
+    #---------------------------------------------------------------------------------------- deliver TAB
     def deliverTab(self):
         mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.tabCtrl_A)
         mc.button(l='Compile bundle',c=self.IO.compile_bundle)
@@ -147,6 +232,7 @@ class dishEditor:
     def __init__(self):
          self.canvasSize =[430,400]
          self.IO =  IO()
+         self.InfosTab = ''
     def widget(self,widgetParent):
         mc.columnLayout( adjustableColumn=True, rs=5 ,p=widgetParent)
         mc.rowLayout(   numberOfColumns=3, 
@@ -175,13 +261,40 @@ class dishEditor:
         mc.scrollLayout(	horizontalScrollBarThickness=0,verticalScrollBarThickness=8)
         scrollWdth = mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0]/5*2-27,h=self.canvasSize[1]-20)
         dishList = self.IO.exposeZipTemplate()
+        
+        dishName = ''
         for dish in dishList:
             dishName = os.path.basename(dish).split('.zip')[0]
-            mc.button(l=dishName,h=28,ann='test')
+            btn = mc.button(l=dishName,h=28,ann='test')
+            mc.button(btn,e=True,c=partial( self.switch_to_bent, dishName ,dish ))
+
+    def switch_to_bent(self,dishName,dishFile,*args):
+        archive = zipfile.ZipFile(dishFile, 'r')
+        jsonFile = archive.read('dish.ini')
+        jsondata = json.loads(jsonFile)  
+        archive.close()
+
+        #Clear chld
+        chldrn = mc.layout( self.InfosTab, query=True, childArray=True )
+        for chld in chldrn:
+            mc.deleteUI(chld)
+
+        mc.columnLayout( adjustableColumn=True ,p=self.InfosTab )
+        mc.text( 'CCW_TOOLS_DISHNAME_txFld', l='', h=36  )
+        #print jsondata['moduleInfos']
+        ll = """<html>
+            <body>
+            <h1>%s</h1></body>
+        </html>
+        """%(dishName )
+        mc.text( 'CCW_TOOLS_DISHNAME_txFld',e=True,l=ll,font='boldLabelFont')        
+        mc.scrollField( editable=False, wordWrap=True, text=jsondata['moduleInfos'] ,h=120)
+        mc.separator()
     def bentosFactory_UI(self,anchor):
         rgtTab = mc.tabLayout( p=anchor,innerMarginWidth=5, innerMarginHeight=5,h=self.canvasSize[1]-52)
-        mc.frameLayout(   mw=5,labelVisible=False,mh=5,p=rgtTab )
+        self.InfosTab = mc.frameLayout(   mw=5,labelVisible=False,mh=5,p=rgtTab )
         mc.text(l='')
+
         
         #--------------------------------------------------------
         mc.frameLayout(  mw=5,labelVisible=False,mh=5,p=rgtTab )
@@ -195,6 +308,9 @@ class dishEditor:
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[1,'Infos'])
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[2,'Tools'])
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[3,'Member list'])
+        
+        dishList = self.IO.exposeZipTemplate()
+        self.switch_to_bent( os.path.basename(dishList[0]).split('.zip')[0],dishList[0] ) 
 class Tool:
     def __init__(self):
         self.windowRef ='CCW_bentoUI'
@@ -225,7 +341,8 @@ class Tool:
 class IO:
     def compile_bundle(self,*args):
         templateList = self.exposeBundles()
-        if templateList is not None or len(templateList) > 0:
+
+        if templateList is not None and len(templateList) > 0:
             mc.progressBar( 'CCW_TOOLS_bundle_prgrss',
             edit=True,
             beginProgress=True,
@@ -243,9 +360,10 @@ class IO:
                 self.createBundle(tmpleDir,zipPath,tmpleRoot)
                 mc.progressBar('CCW_TOOLS_bundle_prgrss', edit=True, step=1)
             mc.progressBar('CCW_TOOLS_bundle_prgrss', edit=True, endProgress=True)
-            UI().show()
+            Tool().show()
     def createBundle(self,sourcePath,outputZip,root):
         myzip = zipfile.ZipFile(outputZip, 'w') 
+        myzip.write( os.path.join( sourcePath ,'dish.ini') ,  r'\dish.ini'  )
         myzip.write( os.path.join( sourcePath ,'widget.py') ,  r'\widget.py'  )
         myzip.write( os.path.join( sourcePath ,'data.ma') ,  r'\data.ma') 
         myzip.close()
@@ -261,13 +379,16 @@ class IO:
         modulePath = os.path.join(modulePath,'src')
 
         dirlist= os.listdir(modulePath)
-        filteredDirlist = []
+        if dirlist is not None and len(dirlist)> 0:
+            filteredDirlist = []
 
-        for dir in dirlist:
-            if os.path.isdir(os.path.join(modulePath,dir)) == True:
-                filteredDirlist.append(os.path.join(modulePath,dir,'data.ma'))
+            for dir in dirlist:
+                if os.path.isdir(os.path.join(modulePath,dir)) == True:
+                    filteredDirlist.append(os.path.join(modulePath,dir,'data.ma'))
 
-        return filteredDirlist
+            return filteredDirlist
+        else:
+            return None
     def merge(self,sourcePath,prfxToReplace,newModuleName):
         ## ------------------------------- first create temp module
         scrpDir = mc.internalVar(userScriptDir=True)
@@ -290,9 +411,68 @@ class IO:
     def clean_asset_file():
         pass
 class factory:
-    def expose_members():
-        pass
-    def process_root():
-        pass
-    def publish_driver():
-        pass
+    def expose_members(selfroot,
+                            root,
+                            assetListing ):
+        if mc.objExists(root)==True:
+            attributeList = [ 'element']
+
+            for index, attr in enumerate(attributeList):
+                if mc.attributeQuery(attributeList[index],node=root,ex=True) == False:
+                    mc.addAttr(root,ln=attributeList[index] ,m=True,h=True,k=False)
+
+            for index, elemObj in enumerate(assetListing):
+                try:
+                    mc.connectAttr(elemObj+'.nodeState','%s.%s[%s]'%( root ,attributeList[0], index), f=True)
+                except:
+                    pass
+            OpenMaya.MGlobal.displayInfo( '** Module elements were added to %s '% root )
+    def process_root(self,  root,
+                            foodType,
+                            moduleInfos ):
+        if mc.objExists(root)==True:
+            attributeList = ['foodType','moduleInfos']
+            dataList = [foodType,moduleInfos]
+            for index, attr in enumerate(attributeList):
+                if mc.attributeQuery(attributeList[index],node=root,ex=True) == False:
+                    mc.addAttr(root,ln=attributeList[index],dt='string')
+                mc.setAttr('%s.%s'%(root ,attributeList[index] ),l=False)
+                mc.setAttr('%s.%s'%(root ,attributeList[index] ) ,dataList[index],type='string')
+                mc.setAttr('%s.%s'%(root ,attributeList[index] ),l=True)
+            OpenMaya.MGlobal.displayInfo( '** %s was successfully Flagged'% root )
+    def publish_driver(self,root):
+        modulePath = os.path.dirname(inspect.getfile(self.expose_members))
+        attributeList = ['foodType','moduleInfos','element']
+        
+        foodType = mc.getAttr('%s.%s'%(root,attributeList[0]))
+        directory = os.path.join(os.path.join(modulePath,'src'),foodType)
+
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        targetFile  = os.path.join(directory,'data.ma')
+        filename    = os.path.join(directory,'dish.ini')
+        widgetname    = os.path.join(directory,'widget.py')
+        
+        fo = open(widgetname, "w")
+        fo.close()
+        
+        attributeList = ['foodType','moduleInfos' ]
+        bakedData = {}
+        
+        for attr in attributeList:
+            dat = mc.getAttr('%s.%s'%(root,attr))
+            if dat is None or len(dat)< 1 :
+                dat = []
+            bakedData[attr]= dat 
+        
+        mc.select (root,r=True)       
+        mc.file(targetFile,force=True,typ="mayaAscii",pr=True, es=True)
+        
+        try:
+            jsondata = json.dumps(bakedData ,sort_keys=True, indent=4)
+            fd = open(filename, 'w')
+            fd.write(jsondata)
+            fd.close()
+        except:
+            print 'ERROR writing', filename
+            pass
