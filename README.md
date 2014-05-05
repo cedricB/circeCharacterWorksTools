5e5f03bd0904fa2c6a0d057e164e4f6c94b9ba74
cedric bazillou <cedricbazillou@gmail.com>
2014-05-04 21:29:20 -0500

    update UI organization

--- README.md
+++ README.md
@@ -3,4 +3,25 @@ circeCharacterWorksTools
 
 A set of tool for maya designed to handle character deformation effectively
 
+### Installing
 
+1. Download a [release][]
+2. Unpack into a target folder
+
+### maya environment variable
+ccwLocation = D:\GitHub
+MAYA_PLUG_IN_PATH = %ccwLocation%\circeCharacterWorksTools\maya\plug-ins
+
+1. copy the above lines into your local maya.env text file
+2. modify ccwLocation by your target folder
+
+### USE
+1. in your script editor
+import maya.cmds as mc
+import sys, os
+TargetDirectory = 'D:\GitHub'
+if TargetDirectory not in sys.path:
+    sys.path.append(TargetDirectory)
+
+import circeCharacterWorksTools.maya.rig.dish.core as bento
+bento.Tool().show()
--- maya/rig/dish/core.py
+++ maya/rig/dish/core.py
@@ -3,88 +3,182 @@ import maya.cmds as mc
 import maya.OpenMaya as OpenMaya
 import  inspect
 import zipfile
+from functools import partial
 
 author = 'cedric bazillou 2013'
-version = 0.02
+version = 0.03
 
-class UI:
+class dishBuilder:
     def __init__(self):
-        self.windowRef ='CCW_bentoUI'
-        self.tmpleLib =  IO()
+        self.tabCtrl_A = ''
+        self.tabCtrl_B = ''
         self.canvasSize =[430,400]
-        self.UI_TAB = ''
-        self.ToDelete = []
-        self.toresizeCtrl = ''
-        self.toresizeCtrlB = ''
-    def resizeTab_In_Compile(self):
-        tabIndex = mc.tabLayout(self.toresizeCtrl ,q=True,sti=True )
+        self.bundle_prgrss = ''
+        self.bentoElements = 'CCW_TOOLS_bentoElem'
+        self.foodField = ''
+        self.IO =  IO()
+        self.defaultNodes = [u'time1', u'sequenceManager1', u'renderPartition',
+        u'renderGlobalsList1', u'defaultLightList1', u'defaultShaderList1',
+        u'postProcessList1', u'defaultRenderUtilityList1',
+        u'defaultRenderingList1', u'lightList1',
+        u'defaultTextureList1', u'lambert1', u'particleCloud1',
+        u'initialShadingGroup', u'initialParticleSE', u'initialMaterialInfo',
+        u'shaderGlow1', u'dof1', u'defaultRenderGlobals', 
+        u'defaultRenderQuality', u'defaultResolution', u'defaultLightSet',
+        u'defaultObjectSet', u'defaultViewColorManager', u'hardwareRenderGlobals',
+        u'hardwareRenderingGlobals', u'characterPartition', u'defaultHardwareRenderGlobals',
+        u'ikSystem', u'hyperGraphInfo', u'hyperGraphLayout', u'globalCacheControl', 
+        u'dynController1', u'strokeGlobals', u'CustomGPUCacheFilter', u'objectTypeFilter74',
+        u'persp', u'perspShape', u'top', u'topShape', u'front', u'frontShape', u'side', u'sideShape',
+        u'lightLinker1', u'layersFilter', u'objectTypeFilter75', u'animLayersFilter',
+        u'objectTypeFilter76', u'notAnimLayersFilter', u'objectTypeFilter77',
+        u'defaultRenderLayerFilter', u'objectNameFilter4', u'renderLayerFilter',
+        u'objectTypeFilter78', u'objectScriptFilter10', u'renderingSetsFilter',
+        u'objectTypeFilter79', u'relationshipPanel1LeftAttrFilter', u'relationshipPanel1RightAttrFilter',
+        u'layerManager', u'defaultLayer', u'renderLayerManager', u'defaultRenderLayer'] # 
+
+    #---------------------------------------------------------------------------------------- widget Helpers 
+    def resizeBuildTabs(self):
+        tabIndex = mc.tabLayout(self.tabCtrl_A ,q=True,sti=True )
         if tabIndex == 1 :
-             mc.tabLayout(self.toresizeCtrl,e=True,  h=592 )
+             mc.tabLayout(self.tabCtrl_A,e=True,  h=618 )
         if tabIndex == 2 :
-            mc.tabLayout(self.toresizeCtrl,e=True,  h=80 )
-    def injectElement_Tab(self ):
-        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.toresizeCtrlB)
-        mc.button(l='Scan current scene content')
-        mc.textScrollList( 'CCW_TOOLS_bentoElem',numberOfRows=10, allowMultiSelection=True)
-        mc.button(l='Remove selected',h=36)
+            mc.tabLayout(self.tabCtrl_A,e=True,  h=80 )
+    #---------------------------------------------------------------------------------------- MAIN UI 
+    def widget(self,widgetParent ):
+        mc.text(l=' Current Path :')
+        pathTxFld = mc.textField( 'buildDish_UI_data_dir_txFld',ed=False  )
+        self.tabCtrl_A = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5,p=widgetParent  )
         
-    def defineDriver_Tab(self, ):
-        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.toresizeCtrlB)
-        mc.separator()
-    def flagRoot_tab(self):
+        #---------------------------------------------------------------------------------
+        self.cookTab()
+        #---------------------------------------------------------------------------------
+        self.deliverTab()
+
+        modulePath = ''.join(os.path.dirname(inspect.getfile(self.resizeBuildTabs))+'\src')
+        mc.textField( pathTxFld,e=True,tx=modulePath)        
+        mc.tabLayout(self.tabCtrl_A ,e=True,tabLabelIndex=[1,'Cook'])
+        mc.tabLayout(self.tabCtrl_A ,e=True,tabLabelIndex=[2,'Deliver'])
+        mc.tabLayout(self.tabCtrl_A ,e=True,cc=self.resizeBuildTabs )
+    #---------------------------------------------------------------------------------------- cookTab UI sub Elements
+    def flagRoot_UI(self):
+        mc.rowLayout( numberOfColumns=3, columnWidth3=(80,(self.canvasSize[0]-80-20),20 ), adjustableColumn=2,cl3=('left','both','right'))
+        mc.text(l=' Bento root :')
+        txFld = mc.textField( 'CCW_TOOLS_ROOT_data_dir_txFld' )
+        psdFileRootBtn = mc.button(l='<' ,c=partial(self.collect_root, txFld ) )
+        mc.setParent('..')
+
         mc.text(l='FoodType')
-        mc.textField( 'CCW_TOOLS_FoodType_txFld', )
+        self.foodField = mc.textField( 'CCW_TOOLS_FoodType_txFld',alwaysInvokeEnterCommandOnReturn=True )
+        mc.textField( self.foodField,e=True, enterCommand=partial(self.validateModule, self.foodField  )  )
         mc.separator()
         mc.text(l='Description')
         mc.scrollField( editable=True, wordWrap=True, text='#Any relevant informations can be placed here' )
         mc.button(l='Flag bento root')
         mc.separator()
-    def bundleTab(self):
-        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.toresizeCtrl)
-        mc.button(l='Compile bundle',c=self.tmpleLib.compile_bundle)
-        mc.progressBar('CCW_TOOLS_bundle_prgrss')
+    def injectElement_UI(self ):
+        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.tabCtrl_B)
+        mc.button(l='Scan current scene content',c=self.scanSceneGraph)
+        mc.textScrollList( 'CCW_TOOLS_bentoElem',numberOfRows=10, allowMultiSelection=True)
+        mc.button(l='Remove selected',h=36,c=self.removeNodeFromElementList)
+    def defineDriver_UI(self ):
+        mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5 ,p=self.tabCtrl_B)
+        mc.separator(style='none')
+    #---------------------------------------------------------------------------------------- cookTab UI Helpers
+    def validateModule(self ,fieldRef,*args):
+        foodTx = mc.textField( self.foodField,q=True,tx=True)
+        if foodTx is not None:
+            modulePath = ''.join(os.path.dirname(inspect.getfile(self.resizeBuildTabs))+'\src')
+            filelist= os.listdir(modulePath)
+            if foodTx in  filelist:
+            
+                prt = mc.promptDialog(    title='foodType Alert', 
+                                    message='Please choose another foodType name:',
+                                    button=['OK'],
+                                    defaultButton='OK')
+                mc.textField( self.foodField,e=True,tx='')
+            
+    def collect_root(self,fieldRef,*args):
+        sel = mc.ls(sl=True,fl=True)
+        mc.textField(fieldRef,e=True,tx='')
+        if sel is None or len(sel)<1  :                
+            return 
+        else:
+            mc.textField(fieldRef,e=True,tx=sel[0])
+    def scanSceneGraph(self ,*args):
+        sceneGraph = mc.ls()
+        mc.textScrollList(self.bentoElements,e=True,ra=True)
         
+        filterList = [ item for item in sceneGraph if item not in self.defaultNodes]
         
-        mc.tabLayout(self.toresizeCtrl ,e=True,tabLabelIndex=[1,'Cook'])
-        mc.tabLayout(self.toresizeCtrl ,e=True,tabLabelIndex=[2,'Deliver'])
-        mc.tabLayout(self.toresizeCtrl ,e=True,cc=self.resizeTab_In_Compile )
-    def CompileTab(self):
-        mc.text(l=' Current Path :')
-        pathTxFld = mc.textField( 'data_dir_txFld',ed=False  )
-        self.toresizeCtrl = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5,p=self.UI_TAB  )
-        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.toresizeCtrl)
-        anchorA = mc.frameLayout( collapsable=False,labelVisible=False, mw=5,mh=5,borderStyle='out' )
+        for obj in filterList:
+            mc.textScrollList(self.bentoElements,e=True,a=obj)
+    def removeNodeFromElementList(self ,*args):
+        selectedItems = mc.textScrollList(self.bentoElements,q=True,selectItem=True)
+        if selectedItems is not None:
+            selectedItems.reverse()
+            for obj in selectedItems :
+                mc.textScrollList(self.bentoElements,e=True,removeItem=obj)
+    #---------------------------------------------------------------------------------------- cook TAB
+    def cookTab(self):
+        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.tabCtrl_A)
+        buildDish_UI_Anchor = mc.frameLayout( collapsable=False,labelVisible=False, mw=5,mh=5,borderStyle='out' )
         
-        self.flagRoot_tab()        
-        self.toresizeCtrlB = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5 )        
-        self.injectElement_Tab() 
-        self.defineDriver_Tab()
+        self.flagRoot_UI()        
+        self.tabCtrl_B = mc.tabLayout( innerMarginWidth=5, innerMarginHeight=5 )        
+        self.injectElement_UI() 
+        self.defineDriver_UI()
 
-        
-        mc.setParent(anchorA)
+        # publish Elements
+        mc.setParent(buildDish_UI_Anchor)
         mc.separator()
         mc.button(l='Release bento',h=45)
         
-        #---------------------------------------------------------------------------------
-        self.bundleTab()
-        
+        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[1,'Flag Elements'])
+        mc.tabLayout(self.tabCtrl_B ,e=True,tabLabelIndex=[2,'Expose Drivers'])
+    #---------------------------------------------------------------------------------------- deliverTab UI Helpers
+    
+    #---------------------------------------------------------------------------------------- cook TAB
+    def deliverTab(self):
+        mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0],p=self.tabCtrl_A)
+        mc.button(l='Compile bundle',c=self.IO.compile_bundle)
+        self.bundle_prgrss = mc.progressBar('CCW_TOOLS_bundle_prgrss')
+class dishEditor:
+    def __init__(self):
+         self.canvasSize =[430,400]
+         self.IO =  IO()
+    def widget(self,widgetParent):
+        mc.columnLayout( adjustableColumn=True, rs=5 ,p=widgetParent)
+        mc.rowLayout(   numberOfColumns=3, 
+                        columnWidth2=(80,(self.canvasSize[0]-80-20)),
+                        adjustableColumn=2,cl2=('left','both' )  )
+        mc.text(l=' Current Path :')
+        pathTxFld = mc.textField( 'buildEditor_UI_data_dir_txFld',ed=False  )
+        mc.setParent('..')
+        mc.separator()
+        anchorDock = mc.rowLayout(    numberOfColumns=2,
+                                    columnWidth2=(self.canvasSize[0]/5*2+12, 75 ),
+                                    adjustableColumn=2,
+                                    columnAlign=(1, 'right'),
+                                    columnAttach=[(1,'both',0), (2,'both',0)] ,w=self.canvasSize[0])
 
-        modulePath = ''.join(os.path.dirname(inspect.getfile(self.bentoListing))+'\src')
+        self.exposedBentos_UI(anchorDock)
+        #------------------------------------------------------------------------------------------------------------
+        self.bentosFactory_UI(anchorDock)
+
+        modulePath = ''.join(os.path.dirname(inspect.getfile(self.exposedBentos_UI)))
         mc.textField( pathTxFld,e=True,tx=modulePath)
-        
-        mc.tabLayout(self.toresizeCtrlB ,e=True,tabLabelIndex=[1,'Flag Elements'])
-        mc.tabLayout(self.toresizeCtrlB ,e=True,tabLabelIndex=[2,'Expose Drivers'])
-    def leftPanel_in_bentoListing(self,anchor):
+    def exposedBentos_UI(self,anchor):
         leftPanel = mc.frameLayout( collapsable=False,labelVisible=False, borderStyle='etchedIn',mw=5,mh=5,p=anchor )
         mc.columnLayout( adjustableColumn=True, rs=5 )
         mc.text(l='Available Bentos')
         mc.scrollLayout(	horizontalScrollBarThickness=0,verticalScrollBarThickness=8)
         scrollWdth = mc.columnLayout( adjustableColumn=True, rs=5,w=self.canvasSize[0]/5*2-27,h=self.canvasSize[1]-20)
-        bndList = self.tmpleLib.exposeZipTemplate()
-        for n in range(len(bndList) ):
-            templateCls = os.path.basename(bndList[n]).split('.zip')[0]
-            mc.button(l=templateCls,h=28,ann='test')
-    def rigthPanel_in_bentoListing(self,anchor):
+        dishList = self.IO.exposeZipTemplate()
+        for dish in dishList:
+            dishName = os.path.basename(dish).split('.zip')[0]
+            mc.button(l=dishName,h=28,ann='test')
+    def bentosFactory_UI(self,anchor):
         rgtTab = mc.tabLayout( p=anchor,innerMarginWidth=5, innerMarginHeight=5,h=self.canvasSize[1]-52)
         mc.frameLayout(   mw=5,labelVisible=False,mh=5,p=rgtTab )
         mc.text(l='')
@@ -101,31 +195,28 @@ class UI:
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[1,'Infos'])
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[2,'Tools'])
         mc.tabLayout(rgtTab ,e=True,tabLabelIndex=[3,'Member list'])
-    def bentoListing(self):
-        mc.columnLayout( adjustableColumn=True, rs=5 ,p=self.UI_TAB)
-        mc.rowLayout( numberOfColumns=3, columnWidth2=(80,(self.canvasSize[0]-80-20)), adjustableColumn=2,cl2=('left','both' )  )
-        mc.text(l=' Current Path :')
-        pathTxFld = mc.textField( 'data_dir_txFld',ed=False  )
-        mc.setParent('..')
-        mc.separator()
-        listDock = mc.rowLayout( numberOfColumns=2, columnWidth2=(self.canvasSize[0]/5*2+12, 75 ), adjustableColumn=2, columnAlign=(1, 'right'), columnAttach=[(1, 'both', 0), (2, 'both', 0)] ,w=self.canvasSize[0])
-
-        self.leftPanel_in_bentoListing(listDock)
-        #------------------------------------------------------------------------------------------------------------
-        self.rigthPanel_in_bentoListing(listDock)
-        modulePath = ''.join(os.path.dirname(inspect.getfile(self.CompileTab)))
-        mc.textField( pathTxFld,e=True,tx=modulePath)
+class Tool:
+    def __init__(self):
+        self.windowRef ='CCW_bentoUI'
+        self.tmpleLib =  IO()
+        self.canvasSize =[430,400]
+        self.UI_TAB = ''
+        self.ToDelete = []
+        self.toresizeCtrl = ''
+        self.toresizeCtrlB = ''
+        self.dishBuilder = dishBuilder()
+        self.dishEditor  = dishEditor()
     def show(self):
         if mc.window(self.windowRef,q=True,ex=True) == True:
             mc.deleteUI(self.windowRef)
         mc.window(self.windowRef , title="CCW_Bento Manager  %s"%version,  widthHeight=(self.canvasSize[0],self.canvasSize[1]),rtf=True,s=False )
  
         self.UI_TAB = mc.frameLayout(cll=False,lv=False,mw=5,w=self.canvasSize[0])
-        bndList = self.tmpleLib.exposeZipTemplate()
-        if len(bndList) == 0:
-            self.CompileTab()
+        dishList = self.tmpleLib.exposeZipTemplate()
+        if len(dishList) == 0:
+            self.dishBuilder.widget(self.UI_TAB)
         else:
-            self.bentoListing()
+            self.dishEditor.widget(self.UI_TAB)
 
         mc.textField( 'copyrigth_txFld',ed=False ,tx=' AUTHOR : cedric BAZILLOU 2014      --       http://circecharacterworks.wordpress.com/ ',p=self.UI_TAB  )
         
@@ -158,8 +249,6 @@ class IO:
         myzip.write( os.path.join( sourcePath ,'widget.py') ,  r'\widget.py'  )
         myzip.write( os.path.join( sourcePath ,'data.ma') ,  r'\data.ma') 
         myzip.close()
-
-
     def exposeZipTemplate(self):
         modulePath = os.path.dirname(inspect.getfile(self.merge))
         filelist= os.listdir(modulePath)
